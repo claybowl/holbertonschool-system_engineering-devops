4a4673c8f2f890f0023063466ecf19b088165fab
Clayton Christian <claybowlproductions@gmail.com>
2022-11-30 23:43:42 -0600

    fix?

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,38 +3,39 @@
 extend module 0 Python script to export
 data in the JSON format.
 """
-import collections
-import csv
 import json
 import requests
 import sys
-if len(sys.argv) != 1:
-    exit(1)
 
-url = 'https://jsonplaceholder.typicode.com/'
 
-urlUser = url + 'users/'
-urlTodos = url + 'todos/'
+def everyone_todo_to_JSON():
+    """This method gathers employee to do information from an API
+    Employee task information is sent to a JSON file
+    """
+    url = "https://jsonplaceholder.typicode.com/"
 
-users = requests.get(urlUser).json()
-todos = requests.get(urlTodos).json()
+    employee_names = requests.get(url + 'users').json()
+    employees_todos = requests.get(url + 'todos').json()
 
-if (len(users) == 0):
-    exit(1)
+    return_dict = {}
+    all_users_dict = {}
 
-data = collections.OrderedDict()
-values = []
+    for employee in employee_names:
+        user_id = employee.get('id')
+        return_dict[user_id] = []
+        all_users_dict[user_id] = employee.get('username')
+    # print(all_users_dict)
+    for task in employees_todos:
+        employee_tasks_dict = {}
+        user_id = task.get('userId')
+        employee_tasks_dict['username'] = all_users_dict.get(user_id)
+        employee_tasks_dict['task'] = task.get('title')
+        employee_tasks_dict['completed'] = task.get('completed')
+        return_dict.get(user_id).append(employee_tasks_dict)
 
-for user in users:
-    username = user.get("username")
-    for todo in todos:
-        t = collections.OrderedDict()
-        t["username"] = username
-        t["task"] = todo.get("title")
-        t["completed"] = todo.get("completed")
-        values.append(t)
-        data["1"] = values
+    with open('todo_all_employees.json', 'w') as JSONFile:
+        json.dump(return_dict, JSONFile)
 
-filename = "todo_all_employees.json"
-with open(filename, "w") as fp:
-    fp.write(json.dumps(data))
+
+if __name__ == "__main__":
+    everyone_todo_to_JSON()
