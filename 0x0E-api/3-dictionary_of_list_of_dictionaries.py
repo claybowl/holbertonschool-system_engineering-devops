d661fa2ee70b3a48536da05a15e81dd9e6aa57aa
Clayton Christian <claybowlproductions@gmail.com>
2022-11-30 23:24:00 -0600

    task 3 attempt

---
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""module 3-dictionary_of_list_of_dictionaries
+extend module 0 Python script to export
+data in the JSON format.
+"""
+import json
+import requests
+import sys
+
+def todo_all_employees():
+    """makes dictionary list of dictionaries"""
+    url = 'https://jsonplaceholder.typicode.com'
+    users = requests.get("{}/users".format(url)).json()
+
+    dict = {}
+    user_dict = {}
+
+    for user in users:
+        user_id = user.get('id')
+        dict[user_id] = []
+        user_dict[user_id] = user.get('username')
+
+    todo = requests.get("{}/todos".format(url)).json()
+
+    for task in todo:
+        task_dict_emp = {}
+        user_id = task.get('userId')
+        task_dict_emp["username"] = user_dict.get(user_id)
+        dict.get(user_id).append(task_dict_emp)
+
+    with open("todo_all_employees.json", 'w') as json_file:
+        json.dump(dict, json_file)
+
+
+if __name__ == "__main__":
+    todo_all_employees()
