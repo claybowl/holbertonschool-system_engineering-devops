453e253adf198cc1c985dfe04f205c5934e8663a
Clayton Christian <claybowlproductions@gmail.com>
2022-11-30 21:28:11 -0600

    task 2 update

--- 0x0E-api/2-export_to_JSON.py
+++ 0x0E-api/2-export_to_JSON.py
@@ -2,29 +2,31 @@
 """module 2-export_to_JSON.py"""
 import json
 import requests
-from sys import argv
+import sys
 
 
-def export_to_json(user_id):
+def export_to_json():
     """exports API data into JSON format"""
+    user_id = sys.argv[1]
+
     url = 'https://jsonplaceholder.typicode.com'
     users_url = '{}/users{}'.format(url, user_id)
+
     username = requests.get(users_url).json().get('username')
-    tasks = requests.get('{}/todos?userID={}'.format(url, user_id)).json()
+    employee_todo = requests.get('{}/todos?userID={}'.format(url, user_id)).json()
 
-    dict = {}
-    dict_list = []
+    employee_dict = {}
+    tasks_list = []
 
-    for task in tasks:
+    for task in employee_todo:
         task_dict = {}
         task_dict["task"] = task.get('title')
         task_dict["completed"] = task.get('completed')
         task_dict["username"] = username
-        dict_list.append(task_dict)
-
-    with open('{}.json'.format(user_id), "w") as json_file:
-        dict[user_id] = dict_list
-        json.dump(dict, json_file)
+        tasks_list.append(task_dict)
+    employee_dict[user_id] = tasks_list
+    with open('{}.json'.format(user_id), "w") as JSONFile:
+        json.dump(dict, JSONFile)
 
 
 if __name__ == "__main__":
