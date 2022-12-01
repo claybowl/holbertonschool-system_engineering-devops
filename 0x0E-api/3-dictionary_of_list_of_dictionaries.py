b7dc2bfde4734b0a670c81fc19640d07ce026fb0
Clayton Christian <claybowlproductions@gmail.com>
2022-11-30 23:35:01 -0600

    fix?

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,26 +12,27 @@ def todo_all_employees():
     """makes dictionary list of dictionaries"""
     url = 'https://jsonplaceholder.typicode.com'
 
-    users = requests.get("{}/users".format(url)).json()
-    todo = requests.get("{}/todos".format(url)).json()
+    employee_names = requests.get(url + 'users').json()
+    employees_todos = requests.get(url + 'todos').json()
 
-    dict = {}
-    user_dict = {}
+    return_dict = {}
+    all_users_dict = {}
 
-    for user in users:
-        user_id = user.get('id')
-        dict[user_id] = []
-        user_dict[user_id] = user.get('username')
+    for employee in employee_names:
+        user_id = employee.get('id')
+        return_dict[user_id] = []
+        all_users_dict[user_id] = employee.get('username')
 
-    for task in todo:
-        task_dict_emp = {}
+    for task in employees_todos:
+        employee_tasks_dict = {}
         user_id = task.get('userId')
-        task_dict_emp["username"] = user_dict.get(user_id)
-        dict.get(user_id).append(task_dict_emp)
+        employee_tasks_dict['username'] = all_users_dict.get(user_id)
+        employee_tasks_dict['task'] = task.get('title')
+        employee_tasks_dict['completed'] = task.get('completed')
+        return_dict.get(user_id).append(employee_tasks_dict)
 
-    with open("todo_all_employees.json", 'w') as json_file:
-        json.dump(dict, json_file)
-
+    with open('todo_all_employees.json', 'w') as JSONFile:
+        json.dump(return_dict, JSONFile)
 
 if __name__ == "__main__":
     todo_all_employees()
